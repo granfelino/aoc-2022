7632c17878a6b9174f339b0e9ce0daa450a3ad64
Karol Partyka <105986108+granfelino@users.noreply.github.com>
2022-12-22 23:02:49 +0100

    added aoc2022_6a and b in Python

---
+++ aoc2022_6/aoc2022_6a.py
@@ -0,0 +1,24 @@
+class aoc2022_6a:
+    def __init__(self):
+        path = "/home/granfelino/own/aoc2022/aoc2022_6/input.txt"
+        file = open(path, "r")
+        self.stream = file.read()
+    
+    def check_marker(self, line):
+        return len(set(line)) == len(line)
+    
+    def parse_input(self):
+        for i in range(len(self.stream) - 4):
+            four = self.stream[i : (i + 4)]
+            if self.check_marker(four):
+                print(self.stream[i : (i + 4)])
+                return i + 1
+
+
+len(set("aaassb"))
+
+
+
+###################
+a = aoc2022_6a()
+print(a.parse_input())
