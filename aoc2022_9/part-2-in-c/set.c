9d8aa13a22d6e005a7e7a3dd7c9c3b77f583b6c0
granfelino <luciusbest@protonmail.com>
2023-01-22 22:41:57 +0100

    Implemented the set architecture and added a header to it to link with the rope structure.

--- aoc2022_9/part-2-in-c/set.c
+++ aoc2022_9/part-2-in-c/set.c
@@ -9,7 +9,7 @@ struct node
   node* next;
 };
 
-node* set_init()
+node* set_init(int* first_entry)
 {
     node* head = (node*) malloc(sizeof(node));
     if (head == NULL) 
@@ -22,11 +22,85 @@ node* set_init()
     }
     
     head->next = NULL;    
+    head->val[0] = first_entry[0];
+    head->val[1] = first_entry[1];
     return head;
 }
 
+int set_check_dupl(node* head, int* arr)
+{
+    node* current = head;
+    while (current->next != NULL)
+    {
+        if ((current->val[0] == arr[0]) && (current->val[1] == arr[1]))
+        {
+            printf("duplicate found");
+            return 1;
+        }    
+        current = current->next;
+    }
+    
+    if ((current->val[0] == arr[0]) && (current->val[1] == arr[1]))
+        {
+            printf("duplicate found\n");
+            return 1;
+        }    
+        
+    
+    
+    printf("\nno duplicates\n");
+    return 0;
+}
+
+void set_push(node* head, int* val_in)
+{
+    if (set_check_dupl(head, val_in)) 
+    {
+        printf("position already saved\n");
+        return;
+    }
+    
+    node* current = head;
+    while (current->next != NULL) current = current->next;
+    
+    current->next = (node*) malloc(sizeof(node));
+    current->next->next = NULL;
+    current->next->val[0] = val_in[0];
+    current->next->val[1] = val_in[1];
+    printf("node added successfully\n");
+}
+
+void set_print(node* head)
+{
+    printf("\nprinting values: \n");
+    node* current = head;
+    while(current->next != NULL)
+    {   
+        printf("(%d, %d) ; ", current->val[0], current->val[1]);    
+        current = current->next;
+    } 
+    
+    printf("(%d, %d) ; ", current->val[0], current->val[1]);    
+}
+
+
+/*
 int main()
 {   
+    int ar1[2] = {0, 0};
+    int ar2[2] = {1, 0};
+    int ar3[2] = {0, 2};
+    int ar4[2] = {3, 0};
+    int ar5[2] = {3, 0};
+
+    node* set = set_init(ar1);
+    set_push(set, ar2);
+    set_push(set, ar3);
+    set_push(set, ar4);
+    set_push(set, ar5);
+    
+    set_print(set);
     
     return 0;
 }
+*/
---
+++ aoc2022_9/part-2-in-c/set.h
@@ -0,0 +1,10 @@
+#ifndef SET_H
+#define SET_H
+
+typedef struct node node;
+node* set_init(int* first_entry);
+int set_check_dupl(node* head, int* arr);
+void set_push(node* head, int* val_in);
+void set_print(node* head);
+
+#endif
